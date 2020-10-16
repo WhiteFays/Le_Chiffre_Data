85185bf56bf1b27bdeaa9311c7869eab989e459a
WhiteFays <maxime.vantorre@hotmail.fr>
2020-10-16 11:45:18 +0200

    Mise en forme SideNav

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function openMenu() {
     document.getElementById('sideMenu').style.marginLeft = '0px';
-    document.getElementById('Bloc').style.marginLeft = '450px';
+    document.getElementById('bloc').style.marginLeft = '0px';
     document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
 }
 
-function closenMenu() {
-    document.getElementById('sideMenu').style.marginLeft = '-450px';
-    document.getElementById('Bloc').style.marginLeft = '0px';
+function closeMenu() {
+    document.getElementById('sideMenu').style.marginLeft = '-350px';
+    document.getElementById('bloc').style.marginLeft = '0px';
     document.body.style.backgroundColor = '#fff';
     
 }
